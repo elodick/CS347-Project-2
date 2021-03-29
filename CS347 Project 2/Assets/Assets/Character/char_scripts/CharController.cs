64e7f7139b724061ad7d5ffe975ef560534f6995
elodick <74278780+elodick@users.noreply.github.com>
2021-03-28 21:34:18 -0500

    adding isometric stuff

---
+++ CS347 Project 2/Assets/Assets/Character/char_scripts/CharController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharController : MonoBehaviour
+{
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
+    public int movementSpeed;
+    Rigidbody2D playerBody;
+
+    void start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        getPlayerInput();
+        Move();
+    }
+    private void getPlayerInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    
+    }
+    void Move()
+    {
+        Vector3 directionVector = new Vector3(horizontalInput, verticalInput, 0);
+        playerBody.velocity = directionVector.normalized * movementSpeed;
+    }
+}
